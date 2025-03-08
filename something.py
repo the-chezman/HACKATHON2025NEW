d2bfe788d148264814968c41b87113b5d25befb3
EVAN <mayimimi331@gmail.com>
2025-03-08 10:10:14 -0500

    Co-authored-by: CHIMMY-CHIMCHIM <CHIMMY-CHIMCHIM@users.noreply.github.com> Co-authored-by: Andrew Close <Andrew-Close@users.noreply.github.com>

---
+++ something.py
@@ -0,0 +1,18 @@
+print("hello")
+
+
+# topic 1: enhance security of tech - 
+# examples include data connectivity, data transfer, 
+# storage, WiFi networks, or password management
+
+#  Ideas:
+# 3 factor authentication
+# Anti-virus
+# Some kind of data storage
+# Encryption/decryption related
+
+
+# topic 2: fight misinformation in social media 
+
+# Ideas:
+# Input a topic and it shows you both sides of the story

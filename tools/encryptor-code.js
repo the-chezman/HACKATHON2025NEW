502b8e37f2f216de41dbe82f6d241dbbf18e62fd
EVAN <mayimimi331@gmail.com>
2025-03-08 17:02:29 -0500

    push7

--- tools/encryptor-code.js
+++ tools/encryptor-code.js
@@ -1,11 +1,11 @@
-const passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!()*$";
+passwordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!()*$";
 
 function generatePassword() {
-    let length = document.getElementById("password-length");
+    let length = document.getElementById("password-length").value;
     let generatedPassword = "";
     for (let i = 0; i < length; i++) {
         let selectedChar = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
         generatedPassword = generatedPassword + selectedChar;
     }
-    document.getElementById("generated-password") = generatedPassword;
+    document.getElementById("newPassword").innerHTML = generatedPassword;
 }
